1bea8c8f7a106353ed4b0eba48533f70e9b20b00
Arthur Purnama <arthur.purnama@astrapay.com>
2021-05-21 13:53:15 +0700

    create test

--- src/test/java/id/purnama/demo/orika/OrikaDemoApplicationTests.java
+++ src/test/java/id/purnama/demo/orika/OrikaDemoApplicationTests.java
@@ -1,13 +1,43 @@
 package id.purnama.demo.orika;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.JsonPathResultMatchers;
+
+import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class OrikaDemoApplicationTests {
 
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
-    void contextLoads() {
+    void test1() throws Exception {
+        mockMvc.perform(get("/sources/1")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content()
+                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.name").value("Bernard"));
+    }
+
+    @Test
+    void test2() throws Exception {
+        mockMvc.perform(get("/sources/1")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content()
+                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.name").value("Boby"));
     }
 
 }
